48dca60eda1d47344b97c3a8036d923496a7f6da
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-10-07 14:06:13 +0200

    create first condition

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,8 +13,11 @@ TOOLS:
 // 1. Scrivi un un programma che stampi i numeri da 1 a 100
   // 1.1 Creo un ciclo for che parta da 1 e arrivi fino a 100 
   for(let i = 1; i <= 100; i++){
-  // 1.2 Controllo se il numero è un multiplo sia di 3 sia di 5
-    // 1.2.1 Se lo è, stampo la scritta FizzBuzz
+    // 1.2 Controllo se il numero è un multiplo sia di 3 sia di 5
+    if(i % 3 === 0 && i % 5 === 0){
+      // 1.2.1 Se lo è, stampo la scritta FizzBuzz
+      console.log("FizzBuzz");      
+    }
   // 1.3 Altrimenti controllo se il numero è divisibile per 3
     // 1.3.1 Se lo è, stampo Fizz
   // 1.4 Altrimenti controllo se il numero è divisibile per 5
